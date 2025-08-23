42bce49c76178e41a58d7992cdbd9a391c73a63a
rofidarashed <rofidaelmaghraby@gmail.com>
2025-08-23 13:40:27 +0300

    add Firebase dependencies and configuration

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:easy_localization/easy_localization.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:kheet_amal/core/routing/app_router.dart';
@@ -7,6 +8,7 @@ import 'package:kheet_amal/core/theme/app_theme.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await EasyLocalization.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(
     EasyLocalization(
       supportedLocales: [Locale('en'), Locale('ar')],
