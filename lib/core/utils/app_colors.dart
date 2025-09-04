1f377762a0e262237b3b9e839402193bad355b63
rofidarashed <rofidaelmaghraby@gmail.com>
2025-09-04 22:36:51 +0300

    add new image assets and update AppImages and AppColors classes

--- lib/core/utils/app_colors.dart
+++ lib/core/utils/app_colors.dart
@@ -1,3 +1,7 @@
+import 'dart:ui';
+
 class AppColors {
-  
+  static const Color primaryColor = Color(0xff92C1EB);
+  static const Color secondaryColor = Color(0xFFFD7E00);
+  static const Color backgroundColor = Color(0xFFFFFFFF);
 }
--- lib/core/utils/app_images.dart
+++ lib/core/utils/app_images.dart
@@ -1,3 +1,4 @@
 class AppImages {
-  
+  static const String kheetAmal = 'assets/images/kheet_amal.png';
+  static const String blob = 'assets/images/blob.png';
 }
