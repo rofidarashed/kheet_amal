1f182300fb4527ca76e8da743650b82d52c4270a
rofidarashed <rofidaelmaghraby@gmail.com>
2025-09-14 17:39:38 +0300

    add logo image

--- lib/core/utils/app_images.dart
+++ lib/core/utils/app_images.dart
@@ -1,4 +1,5 @@
 class AppImages {
   static const String kheetAmal = 'assets/images/kheet_amal.png';
   static const String blob = 'assets/images/blob.png';
+  static const String logo = 'assets/images/logo.png';
 }
