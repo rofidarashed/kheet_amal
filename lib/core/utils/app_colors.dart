5339c109263ed6e67ee77b2b0b02e9eeb7c1cf54
rofidarashed <rofidaelmaghraby@gmail.com>
2025-09-25 12:52:18 +0300

    fix color name

--- lib/core/utils/app_colors.dart
+++ lib/core/utils/app_colors.dart
@@ -4,7 +4,7 @@ class AppColors {
   static const Color primaryColor = Color(0xff92C1EB);
   static const Color secondaryColor = Color(0xFFFD7E00);
   static const Color backgroundColor = Color(0xFFF6F7F8);
-  static const Color hintTextColors = Color(0xff92989B);
+  static const Color hintTextColor = Color(0xff92989B);
   static const Color white = Color(0xFFFFFFFF);
   static const Color black = Color(0xFF000000);
 }
