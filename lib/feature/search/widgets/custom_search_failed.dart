c72e8fa25723950f71cd760985cae903f22fb9e9
AhmedElnesairy <elnesairyahmed@gmail.com>
2025-10-10 15:56:43 +0300

    refactor: remove unused search input and add success message widget

--- lib/feature/search/widgets/custom_search_failed.dart
+++ lib/feature/search/widgets/custom_search_failed.dart
@@ -8,67 +8,6 @@ import 'package:kheet_amal/core/utils/app_icons.dart';
 Widget customSearchfailed({required BuildContext context}) {
   return Column(
     children: [
-      Row(
-        children: [
-          SizedBox(
-            width: 325.w,
-            height: 37.h,
-            child: TextFormField(
-              onTapOutside: (event) => FocusScope.of(context).unfocus(),
-              decoration: InputDecoration(
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(50.r),
-                  borderSide: BorderSide(color: AppColors.primaryColor),
-                ),
-                enabledBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(50.r),
-                  borderSide: BorderSide(color: AppColors.primaryColor),
-                ),
-                focusedBorder: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(50.r),
-                  borderSide: BorderSide(
-                    color: AppColors.primaryColor,
-                    width: 2,
-                  ),
-                ),
-                prefixIcon: IconButton(
-                  icon: Icon(Icons.cancel_outlined, size: 21.sp),
-                  onPressed: () {
-                    FocusScope.of(context).unfocus();
-                  },
-                  color: AppColors.primaryColor,
-                ),
-                suffixIcon: IconButton(
-                  padding: EdgeInsets.only(right: 22.w),
-                  onPressed: () {},
-                  icon: SvgPicture.asset(
-                    AppIcons.searchIcon,
-                    height: 20.h,
-                    width: 20.w,
-                  ),
-                ),
-                hintText: 'search_placeholder'.tr(),
-                hintStyle: TextStyle(
-                  color: AppColors.hintTextColor,
-                  fontSize: 15.sp,
-                ),
-              ),
-            ),
-          ),
-          SizedBox(width: 8.w),
-          GestureDetector(
-            child: Container(
-              height: 37.h,
-              width: 55.w,
-              decoration: BoxDecoration(
-                color: AppColors.secondaryColor,
-                borderRadius: BorderRadius.circular(13.r),
-              ),
-              child: Icon(Icons.tune_rounded, color: AppColors.white),
-            ),
-          ),
-        ],
-      ),
       SizedBox(height: 68.h),
       Center(
         child: Image.asset(
@@ -87,7 +26,6 @@ Widget customSearchfailed({required BuildContext context}) {
           fontWeight: FontWeight.w400,
         ),
       ),
-
       SizedBox(height: 72.h),
       ElevatedButton(
         onPressed: () {},
---
+++ lib/feature/search/widgets/custom_search_success.dart
@@ -0,0 +1,61 @@
+import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:kheet_amal/core/utils/app_colors.dart';
+import 'package:kheet_amal/core/utils/app_icons.dart';
+
+Widget customSearchSuccess({required BuildContext context}) {
+  return Column(
+    children: [
+      SizedBox(height: 30.h),
+      Center(
+        child: SvgPicture.asset(
+          "assets/svgs/search_image.svg",
+          height: 200.h,
+          width: 200.w,
+        ),
+      ),
+      SizedBox(height: 20.h),
+      Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text(
+            "search_message_intro".tr(),
+            style: TextStyle(
+              color: AppColors.black,
+              fontSize: 16.sp,
+              fontWeight: FontWeight.w500,
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SvgPicture.asset("assets/svgs/heart.svg"),
+              Text(
+                "search_message_suffix".tr(),
+                style: TextStyle(
+                  color: AppColors.black,
+                  fontSize: 16.sp,
+                  fontWeight: FontWeight.w500,
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: 64.h),
+          Align(
+            alignment: Alignment.topRight,
+            child: Text(
+              "latest_cases".tr(),
+              style: TextStyle(
+                fontSize: 24.sp,
+                fontWeight: FontWeight.w400,
+              ),
+            ),
+          ),
+          SizedBox(height: 16.h),
+        ],
+      ),
+    ],
+  );
+}
