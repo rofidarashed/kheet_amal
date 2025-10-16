82c7179545c995130664093fcf3436fd66871c4e
AhmedElnesairy <elnesairyahmed@gmail.com>
2025-10-16 16:07:15 +0300

    feat: implement report details screen with various UI components and navigation

---
+++ lib/feature/report_details/widgets/custom_information_card.dart
@@ -0,0 +1,67 @@
+import 'package:easy_localization/easy_localization.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:kheet_amal/core/utils/app_colors.dart';
+import 'package:kheet_amal/feature/home/widgets/info_row.dart';
+
+class InformationPersonCard extends StatelessWidget {
+  const InformationPersonCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: 390.w,
+      height: 245.h,
+      child: Card(
+        shape: RoundedRectangleBorder(
+          side: BorderSide(
+            color: AppColors.primaryColor,
+            width: .5.w,
+          ),
+          borderRadius: BorderRadius.circular(25.r),
+        ),
+        elevation: 0,
+        margin: EdgeInsets.symmetric(horizontal: 12.w),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            InfoRow(label: 'name'.tr(), value: 'احمد'),
+            Divider(
+              color: AppColors.primaryColor,
+              thickness: 1.h,
+              height: 0,
+              indent: 55.w,
+              endIndent: 55.w,
+            ),
+            InfoRow(label: 'age'.tr(), value: '20'),
+            Divider(
+              color: AppColors.primaryColor,
+              thickness: 1.h,
+              height: 0,
+              indent: 55.w,
+              endIndent: 55.w,
+            ),
+            InfoRow(label: 'gender'.tr(), value: "ذكر"),
+            Divider(
+              color: AppColors.primaryColor,
+              thickness: 1.h,
+              height: 0,
+              indent: 55.w,
+              endIndent: 55.w,
+            ),
+            InfoRow(label: 'last_seen_place'.tr(), value: 'هنا'),
+            Divider(
+              color: AppColors.primaryColor,
+              thickness: 1.h,
+              height: 0,
+              indent: 55.w,
+              endIndent: 55.w,
+            ),
+            InfoRow(label: 'missing_date'.tr(), value: '20-20-2020'),
+          ],
+        ),
+      ),
+    );
+  }
+}
