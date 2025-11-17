f626e3f815178972227b071c2af324ced2545986
Ahlam Sultan <ahlame.sultan@gmail.com>
2025-11-17 14:06:38 +0200

    Search

--- lib/feature/search/widgets/custom_search_row.dart
+++ lib/feature/search/widgets/custom_search_row.dart
@@ -6,47 +6,60 @@ import 'package:kheet_amal/core/utils/app_colors.dart';
 import 'package:kheet_amal/core/utils/app_icons.dart';
 import 'package:kheet_amal/feature/filter/filter_screen.dart';
 
-Widget customSearchRow({required BuildContext context}) {
+Widget customSearchRow({
+  required BuildContext context,
+  required TextEditingController controller,
+  required Function(String) onSearch,
+}) {
   return Row(
     children: [
-      SizedBox(
-        width: 325.w,
-        height: 37.h,
-        child: TextFormField(
-          onTapOutside: (event) => FocusScope.of(context).unfocus(),
-          decoration: InputDecoration(
-            border: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(50.r),
-              borderSide: BorderSide(color: AppColors.primaryColor),
-            ),
-            enabledBorder: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(50.r),
-              borderSide: BorderSide(color: AppColors.primaryColor),
-            ),
-            focusedBorder: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(50.r),
-              borderSide: BorderSide(color: AppColors.primaryColor, width: 2),
-            ),
-            prefixIcon: IconButton(
-              icon: Icon(Icons.cancel_outlined, size: 21.sp),
-              onPressed: () {
-                FocusScope.of(context).unfocus();
-              },
-              color: AppColors.primaryColor,
-            ),
-            suffixIcon: IconButton(
-              padding: EdgeInsets.only(right: 22.w),
-              onPressed: () {},
-              icon: SvgPicture.asset(
-                AppIcons.searchIcon,
-                height: 20.h,
-                width: 20.w,
+      Expanded(
+        child: SizedBox(
+          height: 37.h,
+          child: TextFormField(
+            controller: controller,
+            onTapOutside: (event) => FocusScope.of(context).unfocus(),
+            onChanged: (value) {
+              onSearch(value);
+            },
+            decoration: InputDecoration(
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(50.r),
+                borderSide: BorderSide(color: AppColors.primaryColor),
               ),
-            ),
-            hintText: 'search_placeholder'.tr(),
-            hintStyle: TextStyle(
-              color: AppColors.hintTextColor,
-              fontSize: 15.sp,
+              enabledBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(50.r),
+                borderSide: BorderSide(color: AppColors.primaryColor),
+              ),
+              focusedBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(50.r),
+                borderSide: BorderSide(color: AppColors.primaryColor, width: 2),
+              ),
+              prefixIcon: IconButton(
+                icon: Icon(Icons.cancel_outlined, size: 21.sp),
+                onPressed: () {
+                  controller.clear();
+                  onSearch('');
+                  FocusScope.of(context).unfocus();
+                },
+                color: AppColors.primaryColor,
+              ),
+              suffixIcon: IconButton(
+                padding: EdgeInsets.only(right: 22.w),
+                onPressed: () {
+                  onSearch(controller.text);
+                },
+                icon: SvgPicture.asset(
+                  AppIcons.searchIcon,
+                  height: 20.h,
+                  width: 20.w,
+                ),
+              ),
+              hintText: 'search_placeholder'.tr(),
+              hintStyle: TextStyle(
+                color: AppColors.hintTextColor,
+                fontSize: 15.sp,
+              ),
             ),
           ),
         ),
@@ -66,7 +79,6 @@ Widget customSearchRow({required BuildContext context}) {
                 context,
                 MaterialPageRoute(builder: (context) => FilterScreen()),
               );
-
             },
             child: Icon(Icons.tune_rounded, color: AppColors.white),
           ),
